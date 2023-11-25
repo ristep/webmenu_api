afc5bf2005cf00194fe7489bde34fa4f0b430c7d
ristep <ristepan@gmail.com>
2023-11-25 21:45:19 +0100

    from conabo

--- app/settings.py
+++ app/settings.py
@@ -27,10 +27,13 @@ DEBUG = bool(os.environ.get("DEBUG", default=0))
 
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+# ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+# ALLOWED_HOSTS= "wbm-api.sman.cloud conta.sman.cloud sman.cloud 158.220.121.142 localhost 127.0.0.1 [::1] 0.0.0.0"
+ALLOWED_HOSTS = ['wbm-api.sman.cloud', 'localhost', '127.0.0.1', '[::1]', '0.0.0.0' ]
 
 
 CSRF_TRUSTED_ORIGINS = [
+    "https://wbm-api.sman.cloud",
     "http://localhost:1337",
     "http://localhost",
 ]
