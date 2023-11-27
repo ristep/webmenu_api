82d04eae304587ed79fd0f748247c1a94a6c80d1
ristep <ristepan@gmail.com>
2023-11-27 17:55:15 +0100

    Second from cloud

--- app/entrypoint.prod.sh
+++ app/entrypoint.prod.sh
@@ -5,12 +5,10 @@ then
     echo "Waiting for postgres..."
 
     while ! nc -z $SQL_HOST $SQL_PORT; do
-        sleep 0.1
+      sleep 0.1
     done
 
     echo "PostgreSQL started"
 fi
-
-python manage.py collectstatic --noinput
 
 exec "$@"
