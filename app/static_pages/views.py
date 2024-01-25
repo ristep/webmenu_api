f4e5f6a74f89dd4ad882bccdfa0768085db90687
ristep <ristepan@gmail.com>
2024-01-25 09:23:39 +0100

    django_ver

--- app/static_pages/views.py
+++ app/static_pages/views.py
@@ -1,6 +1,6 @@
 # pages/views.py
 from django.views.generic import TemplateView
-
+from django import VERSION
 
 class HomePageView(TemplateView):
     template_name = "home.html"
@@ -8,3 +8,10 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = "about.html"
+    django_ver = VERSION
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["django_version"] = self.django_ver  # Add it to the context
+        context['django_ver'] = ".".join(str(i) for i in VERSION)
+        return context
